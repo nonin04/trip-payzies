765eae13a6084cc439ba50966f3aa3be3e087fb2
lake torrens <laketorrens@lakenoMacBook-Air.local>
2025-05-20 10:55:21 +0900

    tailwind -fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/views/**/*.html.erb",
-    "./app/helpers/**/*.rb",
-    "./app/javascript/**/*.js",
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js'
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
