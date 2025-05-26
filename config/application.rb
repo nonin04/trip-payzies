bed6e2b39df763e9b0bcada60139e2568f344380
nonin04 <nonin.vctr04@gmail.com>
2025-05-26 13:56:10 +0900

    stimulus test2 -add

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module Myapp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    
 
     config.i18n.default_locale = :ja
   end
