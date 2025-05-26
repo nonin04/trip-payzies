dbd174b2abbef867a2154f148612a7f4298f2821
nonin04 <nonin.vctr04@gmail.com>
2025-05-26 19:45:10 +0900

    stimulus test10 -add

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,9 +3,6 @@
 // ./bin/rails generate stimulus controllerName
 
 import { application } from "./application"
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
-
-import SidebarController from "./sidebar_controller"
-application.register("sidebar", SidebarController)
+eagerLoadControllersFrom("controllers", application)
