69559f7e488515d61f3338b6901607d39ba9e3d8
nonin04 <nonin.vctr04@gmail.com>
2025-05-27 13:10:01 +0900

    stimulus test13 -add

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,6 +1,4 @@
-// app/javascript/controllers/application.js
 import { Application } from "@hotwired/stimulus"
-import { definitionsFromContext } from "@hotwired/stimulus-loading"
 
 const application = Application.start()
 application.debug = false
