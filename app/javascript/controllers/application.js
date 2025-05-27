5c4c7661f23b62fb3c40a67548325f67f026b4e2
nonin04 <nonin.vctr04@gmail.com>
2025-05-27 12:44:59 +0900

    stimulus test11 -add

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,13 @@
+// app/javascript/controllers/application.js
 import { Application } from "@hotwired/stimulus"
+import { definitionsFromContext } from "@hotwired/stimulus-loading"
 
 const application = Application.start()
+const context = require.context(".", true, /\.js$/)
+application.load(definitionsFromContext(context))
 
 // Configure Stimulus development experience
 application.debug = false
-window.Stimulus   = application
+window.Stimulus = application
 
 export { application }
