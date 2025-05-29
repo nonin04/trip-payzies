d95c2b18058106ada1c259b09848d5069e4788a3
nonin04 <nonin.vctr04@gmail.com>
2025-05-29 11:02:22 +0900

    root-settings -update

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,22 @@
 Rails.application.routes.draw do
-  resources :expenses
+
+
+  authenticated :user do
+    root 'trips#index', as: :authenticated_root
+  end
+  unauthenticated do
+    root 'homes#index', as: :unauthenticated_root
+  end
+
+
+  resources :trip do
+
+  end
+
+
   devise_for :users
   root to: 'homes#top'
 
-  resources :tasks
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
