35851e7c7431aa81030eb1279a5ca3a3686e8201
nonin04 <nonin.vctr04@gmail.com>
2025-05-29 09:17:36 +0900

    route/tasks-remove&home-fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  root to: 'homes#index'
+
   resources :expenses
   devise_for :users
-  root to: 'homes#top'
 
-  resources :tasks
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+
+
+
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
@@ -14,6 +17,4 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
