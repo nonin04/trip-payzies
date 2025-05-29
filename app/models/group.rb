7b3000ae1b7c83abfaaac7e8819525506eb0eeae
nonin04 <nonin.vctr04@gmail.com>
2025-05-29 23:46:22 +0900

    model's presence-settings -update

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,6 +5,7 @@ class Group < ApplicationRecord
   has_many :members, dependet: :destroy
   
 
+  validates :user_id, presence: true
   validates :name, presence: true, length: {maximum: 15, message: "は15字以内"}
   
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -7,6 +7,8 @@ class Trip < ApplicationRecord
 
   enum settlement_status: {unsettled: 0, settled: 1}
 
+  validates :user_id, presence: true
   validates :titile, presence: true, length: {maximum: 20, message: "20字以内"}
+  validates :settlement_status, presence: true
 
 end
