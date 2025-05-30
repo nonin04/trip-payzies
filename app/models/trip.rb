d7731ce02ed3273ae470a07367a2df093f696bf8
nonin04 <nonin.vctr04@gmail.com>
2025-05-30 10:53:58 +0900

    settlement_default setting -update

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -8,7 +8,15 @@ class Trip < ApplicationRecord
   enum settlement_status: {unsettled: 0, settled: 1}
 
   validates :user_id, presence: true
-  validates :titile, presence: true, length: {maximum: 20, message: "20字以内"}
+  validates :title, presence: true, length: {maximum: 20, message: "20字以内"}
   validates :settlement_status, presence: true
+
+  before_validation :set_default_status
+
+  private
+
+  def set_default_status
+    self.settlement_status ||= :unsettled
+  end
 
 end
