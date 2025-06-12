3db5a90cfe52a403dff02f3fd83267ea36c8259d
nonin04 <nonin.vctr04@gmail.com>
2025-06-12 12:42:43 +0900

    expense edit-page -update

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -10,4 +10,14 @@ class Expense < ApplicationRecord
   validates :title, presence: true, length: {maximum: 20, message: "は20字以内"}
   validates :amount, presence: true, numericality: {only_integer: true, greater_than: 0, less_than: 1_000_000 }
   validates :payer, presence: true
+
+  validate :must_have_owed_participants_at_least_one
+
+  private
+
+  def must_have_owed_participants_at_least_one
+    if advance_payments.empty?
+      errors.add(:base, "対象者を一人以上設定してください")
+    end
+  end
 end
