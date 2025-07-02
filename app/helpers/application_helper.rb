a5be6947d894ca6f363a8608d4353286e469060a
nonin04 <nonin.vctr04@gmail.com>
2025-07-02 11:17:11 +0900

    clean:view内のロジックをhelperに切り出し

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def display_menu?
+    unless devise_controller? || controller_path.start_with?("share/")
+      true
+    else
+      false
+    end
+  end
+
+  def shared_user?
+    controller_path.start_with?("share/")
+  end
+
 end
