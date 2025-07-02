61876098b2c713b86b5d4cfeb5d71672ce5b1140
nonin04 <nonin.vctr04@gmail.com>
2025-07-02 16:00:37 +0900

    aadd:gem ransack

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,6 +2,7 @@ class TripsController < ApplicationController
   before_action :set_trip, only: [ :show, :edit, :update, :destroy, :insights, :result, :settle, :unsettle ]
 
   def index
+    
     @trips = current_user.trips.order(departure_date: :desc)
   end
 
