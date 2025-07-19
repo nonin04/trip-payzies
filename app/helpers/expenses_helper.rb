0a40ec81fb96a5dec93a0da68185f21183cd110f
nonin04 <nonin.vctr04@gmail.com>
2025-07-19 21:52:05 +0900

    change:通貨周りのview修正

--- app/helpers/expenses_helper.rb
+++ app/helpers/expenses_helper.rb
@@ -1,2 +1,31 @@
 module ExpensesHelper
+
+  def selectable_currency(trip)
+    jpy = Currency.find_by(code: "JPY")
+    if trip.currency == jpy
+      [jpy]
+    else
+      [jpy, trip.currency]
+    end
+  end
+
+  def trip_in_japan?(trip)
+    trip.currency == Currency.find_by(code: "JPY")
+  end
+
+  def formatted_amount_local(expense)
+    jpy = Currency.find_by(code: "JPY")
+    if expense.currency == jpy
+      number_with_delimiter(expense.amount_local.to_i)
+    else
+      number_with_precision(expense.amount_local, precision: expense.currency.decimal_position, delimiter: ",")
+    end
+  end
+
+
+  def expense_amount_color(expense)
+    jpy = Currency.find_by(code: "JPY")
+    expense.currency == jpy ? "expense-accent-color-jpy" : "expense-accent-color-foreign"
+  end
+
 end
