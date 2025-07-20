4a25eaf6ab95ee6ffdc4ceeec5f74bf010d72bca
nonin04 <nonin.vctr04@gmail.com>
2025-07-20 18:38:57 +0900

    delete:activestorage削除

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,8 +4,6 @@ class Group < ApplicationRecord
   has_many :trips, dependent: :nullify
   has_many :members, dependent: :destroy
 
-  has_one_attached :icon
-
   validates :user_id, presence: true
   validates :name, presence: true, length: { maximum: 25, message: "は25字以内で入力してください" }
 end
