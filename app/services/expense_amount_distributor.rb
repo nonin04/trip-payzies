d14055f122e310d3fc63c71f140b6ca84c8cebab
nonin04 <nonin.vctr04@gmail.com>
2025-07-25 10:47:49 +0900

    add:立替負担額ロジックモーダル追加

--- app/services/expense_amount_distributor.rb
+++ app/services/expense_amount_distributor.rb
@@ -7,36 +7,19 @@ class ExpenseAmountDistributor
     advance_payments = @expense.advance_payments.to_a
     total_amount = @expense.amount
     count = advance_payments.count
-    if count == 0
-      puts "---------cuontが0です---------"
-      return
-    end
-
-    #------------------------
+    # 総額が対象者より少ないときはreturn
     if total_amount < count
-      puts "---------総額#{total_amount}では1人あたり最低1円ずつ配れないためスキップ------------"
       return
     end
-    #------------------------
 
     # 一人あたりの額(均等分け)
     base_amount = total_amount / count
     # 残り
     remains_amount = total_amount - base_amount * count
 
-    #------------------------
-    puts "▶ 基本額: #{base_amount}, 余り: #{remains_amount}"
-    #------------------------
-
     # 均等分けした額を対象者のamountに追加
     advance_payments.each do |ap|
       ap.amount = base_amount
-      #------------------------
-      puts " - : #{ap.participant.name}"
-      if ap.amount <= 0
-        puts "⚠ #{ap.participant.name} の支払い額が0以下です: #{ap.amount}////////////////"
-      end
-      #------------------------
     end
 
     if remains_amount > 0
@@ -46,17 +29,12 @@ class ExpenseAmountDistributor
       # 選択された負担者のamountに余りを追加
       selected_participant_index.each do |sp|
         advance_payments[sp].amount += 1
-        #------------------------
-        puts " +1: #{advance_payments[sp].participant.name}"
-        #------------------------
       end
     end
-
 
 
     advance_payments.each do |ap|
       ap.save!
     end
-    puts "---------------------------------------"
   end
 end
