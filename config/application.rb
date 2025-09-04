e8a67a3f9cb4d22231a42bb5e6ad34c3eb25c5bb
nonin04 <nonin.vctr04@gmail.com>
2025-09-04 14:08:20 +0900

    fix:ciバグ修正

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,6 @@ module Myapp
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
-    config.autoload_paths << Rails.root.join("app/decorators").to_s
 
     # エラー表示
     # config.exceptions_app = self.routes
